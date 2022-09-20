2aa8d2e0068edba106dfe0f11de0d945d99ed9ee
DanielCosta30 <nielcosta24@gmail.com>
2022-09-19 22:42:27 -0300

    feat(Componets) add NavB

---
+++ src/components/NavB.vue
@@ -0,0 +1,26 @@
+<template>
+
+
+    <div id="nav">
+      <router-link to="/">
+        <img id="logo" :src="logo" :alt="alt">
+      </router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+
+
+</template>
+
+<script>
+
+export default {
+        name: 'NavB'
+
+    }
+</script>
+
+<style scoped>
+
+
+</style>
