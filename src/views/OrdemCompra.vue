bef0414b4d2782a46c76be8cb8b12b7764ac7647
DanielCosta30 <nielcosta24@gmail.com>
2022-09-22 00:09:03 -0300

    feat(OrdemCompra.vuews) add components-perdidoform

--- src/views/OrdemCompra.vue
+++ src/views/OrdemCompra.vue
@@ -2,13 +2,19 @@
   
   <div class="main-container">
     <h1>Ordem de Compra</h1>
+    <PedidoForm />
+
+
   </div>
   </template>
   
   <script>
+import PedidoForm from '@/components/PedidoForm.vue';
+    
 
     export default {
-      name:"OrdemCompra"
-    }
+    name: "OrdemCompra",
+    components: { PedidoForm }
+}
 
   </script>
