0afcbce69fc3750da44d91e0f6b18899983ffb7b
DanielCosta30 <nielcosta24@gmail.com>
2022-09-21 23:58:42 -0300

    feat(Views) add nova pagina de ordem de compra

---
+++ src/views/OrdemCompra.vue
@@ -0,0 +1,14 @@
+<template>
+  
+  <div class="main-container">
+    <h1>Ordem de Compra</h1>
+  </div>
+  </template>
+  
+  <script>
+
+    export default {
+      name:"OrdemCompra"
+    }
+
+  </script>
