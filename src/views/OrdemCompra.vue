11b332c816abf583d7a97dc694d3c9a64fee4af9
DanielCosta30 <nielcosta24@gmail.com>
2022-09-22 01:26:11 -0300

    feat(views) retirado style do OrdemCompra.vue

--- src/views/OrdemCompra.vue
+++ src/views/OrdemCompra.vue
@@ -1,14 +1,12 @@
 <template>
   
-  <div class="main-container">
     <h1>Ordem de Compra</h1>
     <PedidoForm />
 
-
-  </div>
   </template>
   
   <script>
+    
 import PedidoForm from '@/components/PedidoForm.vue';
     
 
