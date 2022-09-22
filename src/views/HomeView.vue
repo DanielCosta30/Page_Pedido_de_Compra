1a7b39d0595418a52742abd796a44ed2880a6304
DanielCosta30 <nielcosta24@gmail.com>
2022-09-21 23:28:45 -0300

    feat(home.vue) retiramos formulario da page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,6 @@
 <template>
  
  <Banner />
-  <div class="main-container">
-    
-    <h1>Safe Value</h1>
-    <h2>Realize consulta e compras com segurança e menor valores</h2>
-    <h2>Sem sair de casa.</h2>
-
-    <p>Formulário</p>
-    
-  </div>
 
 </template>
 
