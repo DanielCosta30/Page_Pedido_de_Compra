b1fcac883deb535b0f0e3b0a940866a331ada8f9
DanielCosta30 <nielcosta24@gmail.com>
2022-09-23 10:24:52 -0300

    Feat(Message) Add style

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,15 +1,26 @@
 <template>
-    <h1>Componente Mensagem</h1>
-
-</template>
-
-<script>
+    <div class="message-container">
+      <p>{{ msg }}</p>
+    </div>
+  </template>
+  
+  <script>
     export default {
-        name:"Message"
+      name: "Message",
+      props: {
+        msg: String
+      }
     }
-</script>
-
-<style scoped>
-
-
-</style>
+  </script>
+  
+  <style scoped>
+    .message-container {
+      color: #004085;
+      background-color: #cce5ff;
+      border: 2px solid #b8daff;
+      border-radius: 5px;
+      padding: 10px;
+      max-width: 400px;
+      margin: 30px auto;
+    }
+  </style>
